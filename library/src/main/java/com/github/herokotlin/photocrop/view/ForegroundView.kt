3168f2083a381419cf3f81f59e5e972aee975c95
musicode <musicode@qq.com>
2019-01-21 09:42:15 +0800

    update

--- library/src/main/java/com/github/herokotlin/photocrop/view/ForegroundView.kt
+++ library/src/main/java/com/github/herokotlin/photocrop/view/ForegroundView.kt
@@ -1,13 +1,9 @@
 package com.github.herokotlin.photocrop.view
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Paint
 import android.util.AttributeSet
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.widget.FrameLayout
 import com.github.herokotlin.photocrop.R
 import com.github.herokotlin.photocrop.util.Util
@@ -42,14 +38,12 @@ class ForegroundView: FrameLayout {
 
     fun updateImageSize() {
         val size = photoView.imageSize
-        Log.d("photocrop1", "size $size")
         Util.updateSize(imageView, size.width, size.height)
     }
 
     fun updateImageOrigin() {
         val origin = photoView.imageOrigin
         Util.updateOrigin(imageView, origin.x, origin.y)
-        Log.d("photocrop1", "origin $origin")
     }
 
 }
